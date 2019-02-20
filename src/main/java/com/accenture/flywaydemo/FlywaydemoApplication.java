20094e823f41c67116059fa7de86144244721b01
Bastian Klein <bastian.klein@accenture.com>
2019-02-20 17:29:56 +0100

    Initial Commit

---
+++ src/main/java/com/accenture/flywaydemo/FlywaydemoApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.flywaydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywaydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlywaydemoApplication.class, args);
+	}
+
+}
